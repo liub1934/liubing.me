a420d49329bfcad80099d987dad00db868c18ebf
liub1934 <liub1934@gmail.com>
2023-01-13 11:55:45 +0800

    import Catalog

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,10 +1,12 @@
 import { defineClientConfig } from '@vuepress/client'
 import ProjectPanel from './components/ProjectPanel'
 import ProjectPanelItem from './components/ProjectPanel/item'
+import Catalog from 'vuepress-plugin-components/client/components/Catalog'
 
 export default defineClientConfig({
   enhance: ({ app, router, siteData }) => {
     app.component('ProjectPanel', ProjectPanel)
     app.component('ProjectPanelItem', ProjectPanelItem)
+    app.component('Catalog', Catalog)
   }
 })
