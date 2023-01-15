7197c5afdf8f07259a154ed2ce51ba1572714584
liub1934 <liub1934@gmail.com>
2023-01-15 11:58:52 +0800

    update config

--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -2,6 +2,7 @@ import { hopeTheme } from 'vuepress-theme-hope'
 import { sidebarConfig } from './sidebar'
 import { navbarConfig } from './navbar'
 import { AuthorName, Description, HostName, Repo } from './constant'
+import { themePlugins } from './plugins'
 import packageData from '../../package.json' assert { type: 'json' }
 const devDependencies = packageData.devDependencies
 const ThemeVersion = devDependencies['vuepress-theme-hope'].replace('^', 'v')
@@ -47,145 +48,7 @@ export default hopeTheme({
     orange: '#fb9b5f'
   },
   // 内置插件
-  plugins: {
-    // 博客插件配置
-    blog: {
-      timeline: 'archives', // 时间轴访问路径修改成archives
-      // 过滤列表页面
-      filter: ({ filePathRelative, frontmatter }) => {
-        // 舍弃那些不是从 Markdown 文件生成的页面
-        if (!filePathRelative) return false
-        // 舍弃notes文件夹的页面
-        if (filePathRelative.startsWith('notes/')) return false
-        // 舍弃nav文件夹的页面
-        if (filePathRelative.startsWith('nav/')) return false
-        // 舍弃所有的README.md页面
-        if (filePathRelative.endsWith('README.md')) return false
-        // 舍弃那些没有使用默认布局的页面
-        if (frontmatter.home || frontmatter.layout) return false
-        return true
-      }
-    },
-    // 复制代码插件配置
-    copyCode: {
-      showInMobile: true // 手机端显示代码复制
-    },
-    // If you don’t need comment feature, you can remove following option
-    // The following config is for demo ONLY, if you need comment feature, please generate and use your own config, see comment plugin documentation for details.
-    // To avoid disturbing the theme developer and consuming his resources, please DO NOT use the following config directly in your production environment!!!!!
-    // 评论插件配置
-    comment: {
-      /**
-       * Using Giscus
-       */
-      // provider: 'Giscus',
-      // repo: Repo,
-      // repoId: 'R_kgDOIq_bDg',
-      // category: 'General',
-      // categoryId: 'DIC_kwDOIq_bDs4CTP9q'
-
-      /**
-       * Using Twikoo
-       */
-      // provider: "Twikoo",
-      // envId: "https://twikoo.ccknbc.vercel.app",
-
-      /**
-       * Using Waline
-       */
-      provider: 'Waline',
-      serverURL: 'https://waline.liubing.me',
-      search: false
-    },
-    // Disable features you don’t want here
-    // Markdown 增强插件配置
-    mdEnhance: {
-      align: true,
-      attrs: true,
-      chart: true,
-      codetabs: true,
-      container: true,
-      demo: true,
-      echarts: true,
-      figure: true,
-      flowchart: true,
-      gfm: true,
-      imgLazyload: true,
-      imgSize: true,
-      include: true,
-      katex: true,
-      mark: true,
-      mermaid: true,
-      playground: {
-        presets: ['ts', 'vue']
-      },
-      presentation: {
-        plugins: ['highlight', 'math', 'search', 'notes', 'zoom']
-      },
-      stylize: [
-        {
-          matcher: 'Recommended',
-          replacer: ({ tag }) => {
-            if (tag === 'em')
-              return {
-                tag: 'Badge',
-                attrs: { type: 'tip' },
-                content: 'Recommended'
-              }
-          }
-        }
-      ],
-      sub: true,
-      sup: true,
-      tabs: true,
-      vPre: true,
-      vuePlayground: true
-    },
-    // PWA插件配置
-    pwa: false // 暂时关闭PWA功能
-    // pwa: {
-    //   favicon: '/favicon.ico',
-    //   cacheHTML: true,
-    //   cachePic: true,
-    //   appendBase: true,
-    //   themeColor: '#2f54eb',
-    //   update: 'hint',
-    //   apple: {
-    //     icon: '/assets/icon/apple-icon-152.png',
-    //     statusBarColor: 'black'
-    //   },
-    //   msTile: {
-    //     image: '/assets/icon/ms-icon-144.png',
-    //     color: '#ffffff'
-    //   },
-    //   manifest: {
-    //     icons: [
-    //       {
-    //         src: '/assets/icon/chrome-mask-512.png',
-    //         sizes: '512x512',
-    //         purpose: 'maskable',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: '/assets/icon/chrome-mask-192.png',
-    //         sizes: '192x192',
-    //         purpose: 'maskable',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: '/assets/icon/chrome-512.png',
-    //         sizes: '512x512',
-    //         type: 'image/png'
-    //       },
-    //       {
-    //         src: '/assets/icon/chrome-192.png',
-    //         sizes: '192x192',
-    //         type: 'image/png'
-    //       }
-    //     ]
-    //   }
-    // }
-  },
+  plugins: themePlugins,
   // 版权信息是否显示
   copyright: false,
   // 自定义页脚
