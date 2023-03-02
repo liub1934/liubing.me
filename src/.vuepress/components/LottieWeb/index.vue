3bcb0df3b7af712aec7b3f57d90c6cdea23f0fb5
liub1934 <liub1934@gmail.com>
2023-03-02 18:27:28 +0800

    feat: 🎸 update 404 page

---
+++ src/.vuepress/components/LottieWeb/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="lottie-web" ref="animContainer"></div>
+</template>
+
+<script lang="ts" setup>
+import { onBeforeUnmount, onMounted, ref } from 'vue'
+import type { AnimationItem } from 'lottie-web'
+import lottie from 'lottie-web'
+
+const props = defineProps({
+  options: {
+    type: Object,
+    required: true
+  }
+})
+
+const emit = defineEmits(['DOMLoaded'])
+const animContainer = ref<Element | null>(null)
+const anim = ref<AnimationItem | null>(null)
+
+onMounted(() => {
+  anim.value = lottie.loadAnimation({
+    container: animContainer.value as Element,
+    renderer: 'svg',
+    loop: props.options.loop !== false,
+    autoplay: props.options.autoplay !== false,
+    animationData: props.options.animationData,
+    path: props.options.path,
+    rendererSettings: props.options.rendererSettings
+  })
+  anim.value.addEventListener('DOMLoaded', () => {
+    emit('DOMLoaded', anim)
+  })
+})
+
+onBeforeUnmount(() => {
+  if (anim.value && anim.value) {
+    anim.value.destroy()
+  }
+})
+</script>
+
+<style lang="scss"></style>
---
+++ src/.vuepress/layouts/NotFound.vue
@@ -0,0 +1,48 @@
+<template>
+  <NotFound>
+    <div class="not-found-hint">
+      <p class="error-code">404</p>
+      <h1 class="error-title">页面不存在</h1>
+    </div>
+    <LottieWeb
+      :options="options"
+      @DOMLoaded="DOMLoaded"
+      :style="{
+        height: '50vh'
+      }"
+    />
+  </NotFound>
+</template>
+
+<script lang="ts" setup>
+import NotFound from 'vuepress-theme-hope/layouts/NotFound'
+import LottieWeb from '../components/LottieWeb/index.vue'
+import { ref, watch } from 'vue'
+import { useDarkmode } from '@theme-hope/modules/outlook/composables/index'
+
+const { isDarkmode } = useDarkmode()
+const options = ref({
+  path: new URL('./NotFound.json', import.meta.url).href
+})
+
+function DOMLoaded() {
+  watch(
+    isDarkmode,
+    () => {
+      const darkFillColor = '#0d1117'
+      const lightFillColor = '#ffffff'
+      const queryColor = isDarkmode.value ? lightFillColor : darkFillColor
+      const fillColor = isDarkmode.value ? darkFillColor : lightFillColor
+      const $rectList = document.querySelectorAll(
+        `.lottie-web rect[fill="${queryColor}"]`
+      )
+      $rectList.forEach((element) => {
+        element.setAttribute('fill', fillColor)
+      })
+    },
+    { immediate: true }
+  )
+}
+</script>
+
+<style lang="less" scoped></style>
