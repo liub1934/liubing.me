460f2fd7274bc0fedad17c884c9d73a182ecabc6
liub1934 <liub1934@gmail.com>
2023-06-03 23:15:07 +0800

    perf: ⚡️ remove dayjs and add date-fns

--- src/.vuepress/utils/index.ts
+++ src/.vuepress/utils/index.ts
@@ -1,4 +1,6 @@
 import { Base } from './constant'
+import { BlogStartDate } from './constant'
+import { parseISO, intervalToDuration, formatDuration } from 'date-fns'
 
 export const withBase = (path: string) => {
   if (!path) return ''
@@ -8,4 +10,30 @@ export const withBase = (path: string) => {
   } else {
     return path
   }
+}
+
+export function setDurationTime() {
+  const $el = document.getElementById('blog-duration')
+  if (!$el) return
+  const start = parseISO(BlogStartDate)
+  const end = new Date()
+  const duration = intervalToDuration({ start, end })
+  const unitMap = {
+    years: '年',
+    months: '个月',
+    days: '天',
+    hours: '小时',
+    minutes: '分钟',
+    seconds: '秒'
+  }
+  const durationArr: string[] = []
+  Object.keys(duration).forEach((key: string) => {
+    const value = duration[key as keyof Duration]
+    if (value) {
+      const unit = unitMap[key as keyof typeof unitMap]
+      durationArr.push(value + unit)
+    }
+  })
+  const durationText = durationArr.join(' ')
+  $el.innerHTML = durationText
 }
