b7f96cc9afaadde0a4641dbeb15c019af8caf56a
liub1934 <liub1934@gmail.com>
2023-07-31 22:31:34 +0800

    refactor: 💡 update layout

--- src/.vuepress/layouts/NavLayout/TrollStore.vue
+++ src/.vuepress/layouts/NavLayout/TrollStore.vue
@@ -8,9 +8,7 @@
             <div class="hint-container tip">
               <p class="hint-container-title">提示</p>
               <p>
-                以下应用均来自电报交流群，有需要补充的应用可以在下方评论区留言，有时间会上传。
-              </p>
-              <p>
+                以下应用均来自电报交流群，有需要补充的应用可以在下方评论区留言，有时间会上传。<br />
                 TrollStore安装及使用可以参考
                 <a
                   href="/article/other/troll-store-install.html"
@@ -19,6 +17,12 @@
                   该教程
                 </a>
                 或者自行Google搜索。
+              </p>
+            </div>
+            <div class="hint-container danger">
+              <p class="hint-container-title">警告</p>
+              <p>
+                某些修改版应用谨慎登录账号（如微信），账号被封与本站无任何关系，本站只是搬运工。
               </p>
             </div>
           </div>
