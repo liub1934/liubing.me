33c19e5968d5f98deeb47067a80816bcbf213925
liub1934 <liub1934@gmail.com>
2023-09-10 10:09:49 +0800

    perf: ⚡️ build

--- src/.vuepress/configs/vite.config.ts
+++ src/.vuepress/configs/vite.config.ts
@@ -1,10 +1,31 @@
 import { InlineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-const viteOptions: InlineConfig = {
-  plugins: [],
+const viteOptions = {
+  plugins: [
+    // 打包分析插件要放最后面
+    visualizer({
+      filename: 'report.html',
+      open: true,
+      brotliSize: true,
+      gzipSize: true,
+      projectRoot: '/'
+    })
+  ],
   build: {
-    assetsInlineLimit: 0 // 设置为0禁止图片转base64
+    assetsInlineLimit: 0, // 设置为0禁止图片转base64
+    rollupOptions: {
+      // 拆包处理
+      manualChunks: {
+        lottie: ['lottie-web'],
+        splinetool: ['@splinetool/runtime']
+      }
+    }
+  },
+  ssr: {
+    // 取消外部化依赖
+    noExternal: ['lottie-web', '@splinetool/runtime']
   }
-}
+} as InlineConfig
 
 export default viteOptions
