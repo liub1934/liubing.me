0baf1e829aed8a97f44c4761a37062bccb14ce8d
liub1934 <liub1934@gmail.com>
2024-05-01 19:53:54 +0800

    feat: 🎸 dotlottie替换lottie-web

---
+++ src/.vuepress/components/DotLottie/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <canvas
+    class="dot-lottie"
+    :id="id"
+    :style="{
+      width: props.width,
+      height: props.height
+    }"
+  ></canvas>
+</template>
+
+<script lang="ts" setup>
+import { DotLottie, type LoadEvent } from '@lottiefiles/dotlottie-web'
+import { computed, onBeforeUnmount, onMounted, ref, watch } from 'vue'
+import { useDarkmode } from '@theme-hope/modules/outlook/composables/index'
+
+const props = defineProps<{
+  width: string
+  height: string
+  path: string
+}>()
+const emit = defineEmits<{
+  (e: 'load', event: LoadEvent): void
+}>()
+
+const { isDarkmode } = useDarkmode()
+const dotLottie = ref<DotLottie | null>(null)
+const id = computed(() => {
+  return `${+new Date()}-${Math.random()}`
+})
+
+watch(isDarkmode, () => {
+  initDotLottie()
+})
+
+onMounted(() => {
+  initDotLottie()
+  dotLottie.value?.addEventListener('load', loadEvent)
+})
+
+onBeforeUnmount(() => {
+  dotLottie.value?.removeEventListener('load', loadEvent)
+  dotLottie.value?.destroy()
+  dotLottie.value = null
+})
+
+function initDotLottie() {
+  dotLottie.value = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.getElementById(id.value) as HTMLCanvasElement,
+    src: props.path
+  })
+}
+
+function loadEvent(e: LoadEvent) {
+  emit('load', e)
+}
+</script>
+
+<style lang="less" scoped></style>
