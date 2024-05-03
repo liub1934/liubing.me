a0a17c20d57133a80d948fe6864d3e9f82555823
liub1934 <liub1934@gmail.com>
2024-05-03 13:44:33 +0800

    fix: 🐛 blog running time

--- src/.vuepress/utils/util.ts
+++ src/.vuepress/utils/util.ts
@@ -9,3 +9,24 @@ export const withBase = (path: string) => {
     return path
   }
 }
+
+export function calculateRuntime(startDate: string) {
+  const units = [
+    { name: '年', duration: 1000 * 60 * 60 * 24 * 365 },
+    { name: '个月', duration: 1000 * 60 * 60 * 24 * 30 },
+    { name: '天', duration: 1000 * 60 * 60 * 24 },
+    { name: '小时', duration: 1000 * 60 * 60 },
+    { name: '分钟', duration: 1000 * 60 },
+    { name: '秒', duration: 1000 }
+  ]
+  let output = ''
+  let diff = Math.abs(new Date().getTime() - new Date(startDate).getTime())
+  for (let unit of units) {
+    const val = Math.floor(diff / unit.duration)
+    if (val > 0) {
+      output += `${val}${unit.name} `
+      diff %= unit.duration
+    }
+  }
+  return output
+}
